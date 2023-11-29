beae5b9c25c41b6fac4aeed83ced71cdbd9b2f04
esnoftz <149547344+esnoftz@users.noreply.github.com>
2023-11-29 17:24:55 +0000

    Added a Replit statement

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("I love computer science!!!");
-        System.out.println("Wooooooowwwwwww this is insane");
-        System.out.println("HIIII");
+      System.out.println("Hello world!");
+      System.out.println("I love computer science!!!");
+      System.out.println("Wooooooowwwwwww this is insane");
+      System.out.println("HIIII");
+      System.out.println("I'm on Replit!");
     }
 }
