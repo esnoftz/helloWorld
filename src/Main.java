9970265f77b15035f079a0868d05f601453496a0
evangeline.noftz25 <esnoftz@gmail.com>
2023-11-29 11:17:32 -0600

    Added a hi

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("I love computer science!!!");
         System.out.println("Wooooooowwwwwww this is insane");
+        System.out.println("HIIII");
     }
 }
